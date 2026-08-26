d2d62a26c0c358f6df82c7a8349817144f1b9fe2
saurav-patel700 <saurav392003@gmail.com>
2026-08-26 17:50:40 +0530

    datatypes

--- JAVASCRIPT/01index.js
+++ JAVASCRIPT/01index.js
@@ -1,1 +1,17 @@
-console.log("hello")
+const accountId=144553
+let accountemail="saurav@gmail.com"
+var accountpassword="12345"
+accountcity="jaipur"
+let accountstate
+
+accountemail="saurav11@gmail.com"
+var accountpassword="1112345"
+accountcity="patna"
+// accountId=2   not possible 
+console.log(accountId);
+/*
+prefer not to use var
+bcause of issue in block scope and functional scope
+*/
+
+console.log([accountId,accountemail,accountpassword,accountcity,accountstate])
---
+++ JAVASCRIPT/02datatypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// alert(3+3)  we using node js not browser
+
+console.log(3+3)
+console.log("saurav")
+
+let name="saurav"
+let age=22  //number=2 to the power 53
+let istall=false // bool true or false
+let state=null    //null standalone value
+//undefined
+//symbol  unique
+
+
+
+//objectr
+console.log(typeof "hitesh");
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
