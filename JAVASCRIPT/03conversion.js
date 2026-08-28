b4f63de075fe21674c85e39869fe5fcf72f9a311
saurav-patel700 <saurav392003@gmail.com>
2026-08-28 07:24:17 +0530

    add

---
+++ JAVASCRIPT/03conversion.js
@@ -0,0 +1,25 @@
+// let score="33abc"
+// let score=undefined
+// let score=null
+// let score=true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueinnumber=number(score)
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
+// "33"=33
+// "33abc"= NaN
+// true=1,flase=0
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+//operation
+
+let value=3
+console.log(2**10);
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+12+"1"+2);
---
+++ JAVASCRIPT/05datatypesummary.js
@@ -0,0 +1,31 @@
+//primitive
+
+// 7types : string
+// string nunber boolean null undefined symbol 
+// bigint 
+
+//reference type or non primitive
+//array.objects,functions
+
+const score =100
+const scorevalue =100.3
+
+const isloggedin = false
+const outsidetemp=null
+let useremail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id==anotherId);
+
+const Bignum=3243423243n
+const heroes=["shakti","naag"]
+let myobj= {
+    name:"saurav",
+    age:22
+}
+const myfunction=function(){
+    console.log("hello world");
+}
+console.log(typeof outsidetemp);
+console.log(typeof scorevalue);
